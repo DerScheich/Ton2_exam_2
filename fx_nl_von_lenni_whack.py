cbc45ecda3fa47190eed498e3030d0d59684f9b5
lennart.hart <lennart.hart@haw-hamburg.de>
2025-06-09 14:02:55 +0200

    added fx_nl_von_lenni_whack.py

---
+++ fx_nl_von_lenni_whack.py
@@ -0,0 +1,45 @@
+import numpy as np
+import scipy.io.wavfile as wav
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+#a = 1/30 # Eingangsvorverstärkung
+#b =  25 # Arbeitspunkt
+
+
+#input = input.astype(np.float32)
+input = np.arange(0, 30, 0.1)
+print(f"input= {input}")
+
+
+def make_non_linear(input_array, a, b):
+    output_array = []
+    output_array = np.sinh(a*input_array+b)
+    print(f"output_array= {output_array}")
+    return output_array
+
+
+
+def make_plots(x,y, xlabel, ylabel):
+    #Übertragung
+    plt.plot(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title("Übertragung")
+    plt.grid(True)
+    plt.show()
+
+    #Verstärkung
+    plt.plot(x, y/x)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title("Verstärkung")
+    plt.grid(True)
+    plt.show()
+
+a, b = 1, 0
+
+make_plots(input, make_non_linear(input, a, b), "Input", "Output")
+
+#make_plots(input, make_non_linear(input, a, b), "Input", "Output")
