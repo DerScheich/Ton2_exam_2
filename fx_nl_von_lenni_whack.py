9bd7d4ab5d7c7e82f645260bfcb5ca9fb863ce1d
lennart.hart <lennart.hart@haw-hamburg.de>
2025-06-15 19:08:18 +0200

    added beginning of Klirrfaktor

--- fx_nl_von_lenni_whack.py
+++ fx_nl_von_lenni_whack.py
@@ -4,44 +4,47 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 
-#a = 1 # Eingangsvorverstärkung
-#b = 1 # Arbeitspunkt
 
+#input = np.arange(-1.1, 1.1, 0.1)
+#print(f"input= {input}")
 
-#input = input.astype(np.float32)
-input = np.arange(-1.1, 1.1, 0.1)
-print(f"input= {input}")
+a, b = 1.1, 0
+fs=48000
 
-
-def make_non_linear(input_array, a, b):
+def fx_nl(input_array, a, b):
     output_array = []
     output_array = np.sinh(a*input_array+b)
     print(f"output_array= {output_array}")
     return output_array
 
 
-
-def make_plots(x,y, xlabel, ylabel):
+def plot_transfer_functions(input,output, input_label, output_label):
     #Übertragung
-    plt.plot(x, y)
-    plt.plot(x, 1.1*x) #Hilfsgerade, die überstiegen werden muss für Verstärkung >1.1
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.plot(input, output)
+    plt.plot(input, 1.1*input) #Hilfsgerade, die überstiegen werden muss für Verstärkung >1.1
+    plt.xlabel(input_label)
+    plt.ylabel(output_label)
     plt.title("Übertragung")
     plt.grid(True)
     plt.show()
 
     #Verstärkung
-    plt.plot(x, y/x)
-    plt. plot(x, x/x*1.1)
+    plt.plot(input, output/input)
+    plt. plot(input, input/input*1.1)
     plt.xlabel("Input")
     plt.ylabel("Verstärkung")
     plt.title("Verstärkung")
     plt.grid(True)
     plt.show()
 
-a, b = 2, 0
+plot_transfer_functions(input, fx_nl(input, a, b), "Input", "Output")
 
-make_plots(input, make_non_linear(input, a, b), "Input", "Output")
+#Sinus-Signal erstellen
+def generate_sine(freq, duration=1, fs=48000, amplitude=1):
+    t = np.arange(0, duration, 1 / fs)
+    return amplitude * np.sin(2 * np.pi * freq * t)
 
-#make_plots(input, make_non_linear(input, a, b), "Input", "Output")
+#Klirrfaktor
+
+non_linear_sine = fx_nl(generate_sine(500),a,b)
+
